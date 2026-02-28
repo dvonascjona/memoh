0bcef0cae65f1e0da6e98600f9f3a901017b7ab1
Acbox <acbox0328@gmail.com>
2026-03-01 02:40:58 +0800

    fix(web): lint

--- packages/web/src/layout/main-layout/index.vue
+++ packages/web/src/layout/main-layout/index.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="flex">
-    <sidebar-provider :default-open="sidebarDefaultOpen" class="**:data-[sidebar=sidebar]:bg-transparent">
+    <sidebar-provider
+      :default-open="sidebarDefaultOpen"
+      class="**:data-[sidebar=sidebar]:bg-transparent"
+    >
       <slot name="sidebar" />
       <slot name="main" />
     </sidebar-provider>
